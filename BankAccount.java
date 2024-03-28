30907a8407cd2fa4ceb85e9e8bc26a48e2d423dc
Gaurav Mukati <gaurav.mukati99@gmail.com>
2024-03-28 11:48:59 +0530

    commit

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+public class BankAccount {
+
+    private int id;
+    private String name;
+    private double deposit;
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
